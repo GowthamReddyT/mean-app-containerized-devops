07c7f3c9dc4ed9de01bf27a369a2fd1dd518cf57
THOTAMADUGULA GOWTHAM REDDY <146721816+GowthamReddyT@users.noreply.github.com>
2026-02-16 11:58:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
-mongoose.Promise = global.Promise;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const db = {};
-db.mongoose = mongoose;
+const db = require("./app/models");
 
-// IMPORTANT: read from docker environment variable
-db.url = process.env.MONGO_URI || "mongodb://localhost:27017/tasks";
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to the database!"))
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
-db.tutorials = require("./tutorial.model.js")(mongoose);
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Test application." });
+});
 
-module.exports = db;
+require("./app/routes/turorial.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
